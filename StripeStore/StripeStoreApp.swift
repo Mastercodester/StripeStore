b240b682b32781be2baac9af8605291389975262
code <ajacks75@emich.edu>
2024-02-28 15:35:04 -0500

    Initial Commit

---
+++ StripeStore/StripeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripeStoreApp.swift
+//  StripeStore
+//
+//  Created by Sean Carter on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct StripeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
